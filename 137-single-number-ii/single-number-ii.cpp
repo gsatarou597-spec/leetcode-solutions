a2b0a82b7f5c38947b28d515a34974945ac5d1e0
Tanmay Mazumdar <gsatarou597@gmail.com>
2026-07-24 20:51:00 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (10.78%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>num;
+        for(auto i:nums){
+            num[i]++;
+        }
+        for  (auto& j : num) {
+            if (j.second == 1) {
+                return j.first;
+            }
+        }
+        return -1;
+    }
+     
+};
