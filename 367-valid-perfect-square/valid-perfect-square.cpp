d123b1bae2a6aabf88181e16e3a35b7fb66551b0
Tanmay Mazumdar <gsatarou597@gmail.com>
2026-08-10 22:57:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (18.95%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int low=0;
+        int high=num;
+        
+        while(low<=high){
+            long long mid=(low+high)/2;
+            if(mid*mid==num)return true;
+            if(mid*mid>=num){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return false;
+        
+    }
+};
