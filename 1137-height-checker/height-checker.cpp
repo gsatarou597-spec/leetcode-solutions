d58d8a883fefb2820850a68e5a1fd32a508e6b2f
Tanmay Mazumdar <gsatarou597@gmail.com>
2026-08-15 20:07:12 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (32.96%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        int mx=heights[0];
-        for(auto h:heights){
-            mx=max(mx,h);
+        int n=heights.size();
+        int mx=*max_element(heights.begin(),heights.end());
+        vector<int>count(mx+1,0);
+        for(int i=0;i<n;i++){
+            count[heights[i]]++;        
         }
-        vector<int>count(mx+1);
-        vector<int>high(heights.size(),0);
-        for(auto h:heights){
-            count[h]++;
+        for(int i=1;i<count.size();i++){
+            count[i]=count[i]+count[i-1];
         }
-        int idx=0;
-        for(int i=0;i<=mx;i++){
-            while(count[i]>0){
-                high[idx]=i;
-                count[i]--;
-                idx++;
-            }
+        vector<int>output(n);
+        for(int i=n-1;i>=0;i--){
+            output[count[heights[i]]-1]=heights[i];
+            count[heights[i]]--;
         }
-        
-        int cnt = 0;
-        for(int i = 0; i < heights.size(); i++) {
-            if(heights[i] != high[i]) {
-                cnt++;
-            }
+        int cnt=0;
+        for(int i=0;i<n;i++){
+            if(heights[i]!=output[i])cnt++;
         }
-        
         return cnt;
-        
+
         
     }
 };
